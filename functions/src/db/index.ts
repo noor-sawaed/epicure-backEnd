cbc1d251cac95ac1c3bea2bd00d2a4a93d114009
noor sawaed <n.swa3ed.swa3ed@gmail.com>
2023-05-28 21:57:42 +0300

    secrets update gitignore 4

--- functions/src/db/index.ts
+++ functions/src/db/index.ts
@@ -2,9 +2,6 @@
 import mongoose from "mongoose";
 const userPass = require("./sercret");
 const connectDb = async () => {
-  // mongoose.set("strictQuery", true);
-  // await mongoose.connect("mongodb+srv://epicure:pbDIYoy1Zhbusng3@main-projects.djscmg5.mongodb.net/epicuredb");
-  // await mongoose.connect('mongodb://epicure:pbDIYoy1Zhbusng3@localhost:27017/epicuredb');
   mongoose.set("strictQuery", true);
   mongoose.connect(userPass)
   .then(() => console.log('MongoDB connected...'))
